79c1fd9035a696889cba3569d90a7ebdefd5b984
Caio <whoiscaio@protonmail.com>
2021-06-28 13:11:10 -0300

    Mobile Drag and Drop Funcionality Added

--- src/js/draggable-element.js
+++ src/js/draggable-element.js
@@ -6,13 +6,42 @@ const slider = document.querySelector('.slider');
 const filledArea = document.querySelector('.filled-area');
 export const elementStyle = getComputedStyle(element);
 export const sliderStyle = getComputedStyle(slider);
+let sliderWidth = sliderStyle.getPropertyValue('width');
+let rightLimit = parseFloat(sliderWidth.substr(0, sliderWidth.length - 2));
 
-element.addEventListener('mousedown', dragElement);
+element.addEventListener('mousedown', dragElementDesktop);
+element.addEventListener('touchstart', dragElementMobile);
 
-function dragElement(e) {
+function dragElementMobile(e) {
+  element.classList.add('being-dragged');
+
+  window.addEventListener('touchend', leaveElement);
+  window.addEventListener('touchmove', moveElement);
+
+  function moveElement(e) {
+    let touchLocation = e.targetTouches[0];
+    let newPos = touchLocation.pageX - 60;
+
+    if (newPos >= 0 && newPos <= rightLimit) {
+      element.style.left = newPos + 'px';
+    }
+
+    priceUpdate(newPos, rightLimit, billingOption);
+  }
+
+  function leaveElement() {
+    window.removeEventListener('touchstart', dragElementMobile);
+    window.removeEventListener('touchmove', moveElement);
+
+    element.classList.remove('being-dragged');
+  }
+}
+
+function dragElementDesktop(e) {
   element.classList.add('being-dragged');
 
   window.addEventListener('mouseup', leaveElement);
+
   window.addEventListener('mousemove', moveElement);
 
   let currentPos = e.clientX;
@@ -21,14 +50,10 @@ function dragElement(e) {
     let newPos = currentPos - e.clientX;
     let value = elementStyle.getPropertyValue('left');
     let newLeftValue = parseFloat(value.substr(0, value.length - 2)) - newPos;
-    let sliderWidth = sliderStyle.getPropertyValue('width');
-    let rightLimit = parseFloat(sliderWidth.substr(0, sliderWidth.length - 2));
 
-    if(true) {
-      if(newLeftValue >= 0 && newLeftValue <= rightLimit) {
-        element.style.left = newLeftValue + "px";
-        filledArea.style.width = newLeftValue + "px";
-      }
+    if (newLeftValue >= 0 && newLeftValue <= rightLimit) {
+      element.style.left = newLeftValue + 'px';
+      filledArea.style.width = newLeftValue + 'px';
     }
 
     currentPos = e.clientX;
@@ -38,6 +63,7 @@ function dragElement(e) {
 
   function leaveElement() {
     window.removeEventListener('mouseup', leaveElement);
+
     window.removeEventListener('mousemove', moveElement);
     element.classList.remove('being-dragged');
   }
