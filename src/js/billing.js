4b1986cd80653e1776cd5e7cf34a8643ce09697a
Caio <whoiscaio@protonmail.com>
2021-06-28 13:21:11 -0300

    Added Responsivity Improvements on Mobile Devices

--- src/js/billing.js
+++ src/js/billing.js
@@ -1,6 +1,6 @@
-import { sliderStyle } from "./draggable-element.js";
-import { elementStyle } from "./draggable-element.js";
-import { priceUpdate } from "./prices-mechanism.js";
+import { sliderStyle } from './draggable-element.js';
+import { elementStyle } from './draggable-element.js';
+import { priceUpdate } from './prices-mechanism.js';
 
 const toggle = document.querySelector('div.toggle');
 export let billingOption = false;
@@ -8,18 +8,25 @@ export let billingOption = false;
 toggle.addEventListener('click', toggleChanger);
 
 function toggleChanger() {
-  let sliderWidth = parseFloat(sliderStyle.getPropertyValue('width').substr(0, sliderStyle.getPropertyValue('width').length - 2));
-  let elementLeft = parseFloat(elementStyle.getPropertyValue('left').substr(0, elementStyle.getPropertyValue('left').length - 2));
+  let sliderWidth = parseFloat(
+    sliderStyle
+      .getPropertyValue('width')
+      .substr(0, sliderStyle.getPropertyValue('width').length - 2)
+  );
+  
+  let elementLeft = parseFloat(
+    elementStyle
+      .getPropertyValue('left')
+      .substr(0, elementStyle.getPropertyValue('left').length - 2)
+  );
 
-  if(toggle.classList.contains('active')) {
-    toggle.classList.remove('active')
+  if (toggle.classList.contains('active')) {
+    toggle.classList.remove('active');
     billingOption = false;
-  }else{
+  } else {
     toggle.classList.add('active');
     billingOption = true;
   }
-  
-  console.log(elementLeft, sliderWidth);
 
   priceUpdate(elementLeft, sliderWidth, billingOption);
 }
